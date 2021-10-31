9ae6fab4d6079d45a7451e5e93c6c2eedccbeeb1
NSale <gospasrb@yahoo.com>
2021-10-31 23:16:46 +0100

    update models and html form to accept images

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -127,6 +127,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
