ae45ff163fd32e19a2b297dd113ad15edb3f9635
NSale <gospasrb@yahoo.com>
2021-11-03 23:08:48 +0100

    add skill model and profile html

--- users/views.py
+++ users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 # Create your views here.
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    all_profiles = Profile.objects.all()
+    context = {
+        'profiles': all_profiles,
+    }
+    return render(request, 'users/profiles.html', context)
+
+
+def profile(request, pk):
+    profile_selected = Profile.objects.get(id=pk)
+    context = {
+       "profile": profile_selected,
+    }
+    return render(request, 'users/user-profile.html', context)
