f8374ff4a7d8c674e9e46368daaeb4923d7773bd
NSale <gospasrb@yahoo.com>
2021-11-03 23:53:14 +0100

    wiring profile and profiles html pages

--- users/views.py
+++ users/views.py
@@ -13,7 +13,13 @@ def profiles(request):
 
 def profile(request, pk):
     profile_selected = Profile.objects.get(id=pk)
+
+    topSkills = profile_selected.skill_set.exclude(description__exact="")
+    otherSkills = profile_selected.skill_set.filter(description="")
+
     context = {
-       "profile": profile_selected,
+        "profile": profile_selected,
+        "topSkills": topSkills,
+        "otherSkills": otherSkills,
     }
     return render(request, 'users/user-profile.html', context)
