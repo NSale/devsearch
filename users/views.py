569f341b20e8ed2b3f03febca0651dd93a5caa04
NSale <gospasrb@yahoo.com>
2021-11-05 22:44:24 +0100

    custom form for register steo

--- users/views.py
+++ users/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
@@ -42,10 +42,10 @@ def logout_user(request):
 
 def register_user(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
